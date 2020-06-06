a94a286c3dd378b01568817d19b6062b55cf87b1
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-06-06 19:01:52 +0900

    :tada: Add card to first column of the project when labeled

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,74 @@
 import { Application } from "probot"; // eslint-disable-line no-unused-vars
 
-const addComment = `
-  mutation comment($id: ID!, $body: String!) {
-    addComment(input: {subjectId: $id, body: $body}) {
-      clientMutationId
+const PROJECT_FRAGMENT = `
+  name
+  id
+  columns(first: 50) {
+    totalCount
+    nodes {
+      id
+      url
+      lastCards: cards(last: 1, archivedStates: NOT_ARCHIVED) {
+        totalCount
+        nodes {
+          url
+          id
+          note
+        }
+      }
     }
   }
 `;
 
-const projectCards = `
-query getCardAndColumnAutomationCards($issueUrl: URI!) {
-  resource(url: $issueUrl) {
-    ... on Issue {
-      projectCards(first: 100) {
-        nodes {
-          id
-          url
-          column {
-            name
-            id
+// const getCardAndColumnAutomationCards = `
+//   query getCardAndColumnAutomationCards($issueUrl: URI!) {
+//     resource(url: $issueUrl) {
+//       ... on Issue {
+//         projectCards(first: 10) {
+//           nodes {
+//             id
+//             url
+//             column {
+//               name
+//               id
+//             }
+//             project {
+//               ${PROJECT_FRAGMENT}
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
+const getAllProjectCards = `
+  query getAllProjectCards($issueUrl: URI!) {
+    resource(url: $issueUrl) {
+      ... on Issue {
+        id
+        repository {
+          owner {
+            url
+            ${"" /* Projects can be attached to an Organization... */}
+            ... on Organization {
+              projects(first: 10, states: [OPEN]) {
+                nodes {
+                  ${PROJECT_FRAGMENT}
+                }
+              }
+            }
+          }
+          ${"" /* ... or on a Repository */}
+          projects(first: 10, states: [OPEN]) {
+            nodes {
+              ${PROJECT_FRAGMENT}
+            }
           }
         }
       }
     }
   }
-}
 `;
 
 export = (app: Application) => {
@@ -32,19 +76,33 @@ export = (app: Application) => {
   app.on("issues.labeled", async (context) => {
     logger.info("labeled!!!", context);
     const issueUrl = context.payload.issue.html_url;
-    context.github.graphql(addComment, {
-      id: context.payload.issue.node_id,
-      body: "Hello world!",
-    });
-
-    const { resource }: any = await context.github.graphql(projectCards, {
+    const issueId = context.payload.issue.node_id;
+    const { resource }: any = await context.github.graphql(getAllProjectCards, {
       issueUrl: issueUrl,
     });
 
     logger.info("resource!!!", resource);
-    const cardsForIssue = resource.projectCards.nodes;
-    for (const issueCard of cardsForIssue) {
-      logger.info("issueCard!!!", issueCard);
-    }
+    const projects = resource.repository.projects.nodes;
+    const columns: any = [];
+    projects.forEach((pj: any) => {
+      logger.info("project!!!", pj);
+      pj.columns.nodes.forEach((col: any) => {
+        logger.info("column!!!", col);
+        columns.push(col);
+      });
+    });
+    logger.info("multiple columns!!!", columns);
+
+    // すでに追加済みの時は Project already has the associated issue のエラーが出る
+    await context.github.graphql(
+      `
+      mutation createCard($contentId: ID!, $columnId: ID!) {
+        addProjectCard(input: {contentId: $contentId, projectColumnId: $columnId}) {
+          clientMutationId
+        }
+      }
+    `,
+      { contentId: issueId, columnId: columns[0].id }
+    );
   });
 };
