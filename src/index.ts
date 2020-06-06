31f203745c1f0976a1ba625690ce9983b79fa5f1
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-06-06 18:14:33 +0900

    [tmp] Get projects issue cards

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,43 @@ const addComment = `
   }
 `;
 
+const projectCards = `
+query getCardAndColumnAutomationCards($issueUrl: URI!) {
+  resource(url: $issueUrl) {
+    ... on Issue {
+      projectCards(first: 100) {
+        nodes {
+          id
+          url
+          column {
+            name
+            id
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
 export = (app: Application) => {
+  const logger = app.log.child({ name: "projectabot" });
   app.on("issues.labeled", async (context) => {
-    console.log("labeled!!!", context);
+    logger.info("labeled!!!", context);
+    const issueUrl = context.payload.issue.html_url;
     context.github.graphql(addComment, {
       id: context.payload.issue.node_id,
       body: "Hello world!",
     });
+
+    const { resource }: any = await context.github.graphql(projectCards, {
+      issueUrl: issueUrl,
+    });
+
+    logger.info("resource!!!", resource);
+    const cardsForIssue = resource.projectCards.nodes;
+    for (const issueCard of cardsForIssue) {
+      logger.info("issueCard!!!", issueCard);
+    }
   });
 };
