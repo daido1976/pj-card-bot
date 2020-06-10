d57ffe4f602faaab05247a1e9026b7e30a271d7c
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-06-10 09:05:43 +0900

    Split graphql mock response other file

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,41 +2,7 @@ import nock from "nock";
 import myProbotApp from "../src";
 import { Probot } from "probot";
 import payload from "./mocks/issues.labeled.json";
-
-const repositoryOnUser = {
-  node: {
-    owner: {},
-    projects: {
-      nodes: [
-        {
-          columns: {
-            nodes: [
-              {
-                id: "hoge",
-                lastCards: {
-                  nodes: [
-                    {
-                      url: "url",
-                      id: "fuga",
-                      note:
-                        "###### Automation Rules\r\n" +
-                        "\r\n" +
-                        "<!-- Documentation: https://github.com/philschatz/project-bot -->\r\n" +
-                        "\r\n" +
-                        "- `added_label` **wontfix**\r\n" +
-                        "- `new_pullrequest` **repo1** **repo2**\r\n" +
-                        "- `new_issue`",
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        },
-      ],
-    },
-  },
-};
+import { repositoryOnUser } from "./mocks/graphqlResponse";
 
 describe("My Probot app", () => {
   let probot: any;
