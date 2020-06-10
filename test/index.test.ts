5c730a2897c9aa4a947dc649b602316d7d3b30f1
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-06-10 21:41:04 +0900

    Add spec for issue opened

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,7 @@ import myProbotApp from "../src";
 import { Probot } from "probot";
 import issueLabeledPayload from "./mocks/issues.labeled.json";
 import prOpenedPayload from "./mocks/pull_request.opened.json";
+import issueOpenedPayload from "./mocks/issue.opened.json";
 import { repositoryOnUser } from "./mocks/graphqlResponse";
 
 describe("PJ Card Bot integration tests", () => {
@@ -43,6 +44,22 @@ describe("PJ Card Bot integration tests", () => {
     await probot.receive({
       name: "pull_request.opened",
       payload: prOpenedPayload,
+    });
+  });
+
+  it("Creates cards when issue opened", async () => {
+    // query getAllProjectCards
+    nock("https://api.github.com")
+      .post("/graphql")
+      .reply(200, { data: repositoryOnUser });
+
+    // mutation createCard
+    nock("https://api.github.com").post("/graphql").reply(200);
+
+    // Receive a webhook event
+    await probot.receive({
+      name: "pull_request.opened",
+      payload: issueOpenedPayload,
     });
   });
 
