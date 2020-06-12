5e2736294f84895cc552bdb27c978db20afb9fce
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-06-13 08:36:22 +0900

    Split existing test every contxt

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,31 +1,33 @@
 import { parseMarkdownToRules } from "../src/parser";
 
 describe("parseMarkdownToRules", () => {
-  const note: string =
-    "###### Automation Rules\r\n" +
-    "\r\n" +
-    "<!-- Documentation: https://github.com/philschatz/project-bot -->\r\n" +
-    "\r\n" +
-    "- `added_label` **wontfix**\r\n" +
-    "- `new_pullrequest` **repo1** **repo2**\r\n" +
-    "- `new_issue`";
+  describe("when note is valid", () => {
+    const note: string =
+      "###### Automation Rules\r\n" +
+      "\r\n" +
+      "<!-- Documentation: https://github.com/philschatz/project-bot -->\r\n" +
+      "\r\n" +
+      "- `added_label` **wontfix**\r\n" +
+      "- `new_pullrequest` **repo1** **repo2**\r\n" +
+      "- `new_issue`";
 
-  const results: { ruleName: string; ruleArgs: string[] }[] = [
-    {
-      ruleName: "added_label",
-      ruleArgs: ["wontfix"],
-    },
-    {
-      ruleName: "new_pullrequest",
-      ruleArgs: ["repo1", "repo2"],
-    },
-    {
-      ruleName: "new_issue",
-      ruleArgs: [],
-    },
-  ];
+    const results: { ruleName: string; ruleArgs: string[] }[] = [
+      {
+        ruleName: "added_label",
+        ruleArgs: ["wontfix"],
+      },
+      {
+        ruleName: "new_pullrequest",
+        ruleArgs: ["repo1", "repo2"],
+      },
+      {
+        ruleName: "new_issue",
+        ruleArgs: [],
+      },
+    ];
 
-  it("parses markdown to rules", () => {
-    expect(parseMarkdownToRules(note)).toEqual(results);
+    it("parses markdown to rules", () => {
+      expect(parseMarkdownToRules(note)).toEqual(results);
+    });
   });
 });
