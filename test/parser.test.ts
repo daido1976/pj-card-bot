628296c7cbe0d4f1c055b119e5b92168be341e85
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-06-13 13:04:49 +0900

    Add spec for parseMarkdownToRules()

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -30,4 +30,45 @@ describe("parseMarkdownToRules", () => {
       expect(parseMarkdownToRules(note)).toEqual(results);
     });
   });
+
+  describe("when note has invalid title", () => {
+    const note: string =
+      "###### Automation Rules\r\n" +
+      "\r\n" +
+      "<!-- Documentation: https://github.com/philschatz/project-bot -->\r\n" +
+      "\r\n" +
+      "- `added_label` **wontfix**\r\n" +
+      "- `new_pullrequest` **repo1** **repo2**\r\n" +
+      "- `new_issue`";
+
+    const results: { ruleName: string; ruleArgs: string[] }[] = [];
+
+    it("returns empty array", () => {
+      expect(parseMarkdownToRules(note)).toEqual(results);
+    });
+  });
+
+  describe("when note has invalid list", () => {
+    const note: string =
+      "###### PJ Card Bot Rules\r\n" +
+      "- `added_label` **wontfix**\r\n" +
+      "- new_pullrequest repo1 **repo2**\r\n" +
+      "- `new_issue`";
+
+    const results: { ruleName: string; ruleArgs: string[] }[] = [
+      {
+        ruleName: "added_label",
+        ruleArgs: ["wontfix"],
+      },
+      {
+        ruleName: "new_issue",
+        ruleArgs: [],
+      },
+    ];
+
+    // Not implemented yet
+    xit("parses markdown to rules only valid listitem", () => {
+      expect(parseMarkdownToRules(note)).toEqual(results);
+    });
+  });
 });
